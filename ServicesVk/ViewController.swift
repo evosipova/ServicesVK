5fb90e75ca88dfe8c161ab1710fa7ee8d4cb8da3
elizavetaosipova <evosipova_3@edu.hse.ru>
2024-03-28 22:06:47 +0300

    Initial Commit

---
+++ ServicesVk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServicesVk
+//
+//  Created by Elizaveta Osipova on 3/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
